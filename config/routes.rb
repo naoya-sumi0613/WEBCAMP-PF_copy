427399ee785ad1298f81e40dd7a0b4a753eb930d
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-05-10 23:13:11 +0900

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'users/homes#top'
   devise_for :admins
   devise_for :users
+
+  scope module: :users do
+    get 'homes/about'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
