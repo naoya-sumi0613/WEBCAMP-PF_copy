94af9e1bfdb07f655eed23bfe266bb1b1b7a898a
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-05-29 22:18:41 +0900

    Add lightbox2 Remove 404 500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,20 +24,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
-  # 例外処理
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from Exception, with: :render_500
-
-  def render_404
-    render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
-  end
-
-  def render_500
-    render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
-  end
-
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :full_name, :read_last_name, :read_first_name, :read_full_name])
